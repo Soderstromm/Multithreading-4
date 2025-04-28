30b520141c730d5aa7498fcc00dfb1fab147a300
Дмитрий Шведов <49454354+Soderstromm@users.noreply.github.com>
2025-04-28 16:18:25 +0500

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,21 @@
+import org.example.PhoneBook;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAddSingleContact() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add("Петя", "+79991234567"));
+    }
+
+    @Test
+    public void testAddFewContacts() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Петя", "+79991234567");
+        assertEquals(2, phoneBook.add("Паша", "+79001234567"));
+    }
+
 }
