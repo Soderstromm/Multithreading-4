077c0a8e4c8c8f67a2b862cff0ae3069934274eb
Дмитрий Шведов <49454354+Soderstromm@users.noreply.github.com>
2025-04-29 11:53:09 +0500

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,8 +1,18 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.TreeSet;
+
 public class PhoneBook {
 
+    private final TreeSet<String> names = new TreeSet<>();
+    private final HashMap<String, String> nameToNumber = new HashMap<>();
+    private final HashMap<String, String> numberToName = new HashMap<>();
+
     public int add(String name, String number){
-        return 0;
+        names.add(name);
+        nameToNumber.put(name,number);
+        numberToName.put(number, name);
+        return names.size();
     }
 }
